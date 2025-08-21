5576767a19168a51d3da2f4fa886574863994a25
nomaan-07 <nomaan07.dev@gmail.com>
2025-08-21 06:20:50 +0330

    refactor: ProductTitle

--- app/_components/Product/components/ProductTitle.tsx
+++ app/_components/Product/components/ProductTitle.tsx
@@ -8,7 +8,7 @@ function ProductTitle({ faTitle, enTitle, variant }: ProductTitleProps) {
 
   return (
     <div className={isMobile ? "lg:hidden" : "hidden lg:block"}>
-      <h1 className="mb-2 text-sm font-bold sm:mb-4 sm:text-lg md:text-xl lg:text-2xl lg:leading-9">
+      <h1 className="mb-2 text-sm leading-6 font-bold sm:mb-4 sm:text-lg sm:leading-7 md:text-xl md:leading-8 lg:text-2xl lg:leading-9">
         {faTitle}
       </h1>
       <p className="text-default-400 text-xs sm:text-sm">{enTitle}</p>
