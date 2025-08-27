6ff09faadca590a56dc881d2e1b62ea792d51de0
nomaan-07 <nomaan07.dev@gmail.com>
2025-08-27 04:49:19 +0330

    fix: Remove purchase panel sticky behavior to resolve UI flicker

--- app/_components/Product/components/DesktopPurchasePanel.tsx
+++ app/_components/Product/components/DesktopPurchasePanel.tsx
@@ -2,7 +2,6 @@
 
 import QuantityText from "@/app/_components/Common/QuantityText";
 import AddToCartButton from "@/app/_components/Product/components/AddToCartButton";
-import { useScrollDirection } from "@/app/_hooks/useScrollDirection";
 import { useProductsStore } from "@/app/_store/productStore";
 import { LOW_PRODUCT_QUANTITY } from "@/app/_utils/constants";
 import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
@@ -15,18 +14,11 @@ import QuantitySelector from "./QuantitySelector";
 
 function DesktopPurchasePanel() {
   const quantity = useProductsStore((state) => state.quantity);
-  const scrollDirection = useScrollDirection();
 
   if (quantity === 0) return <FinishedPanel />;
 
   return (
-    <Card
-      className="hidden transition-transform duration-500 lg:sticky lg:top-4 lg:z-30 lg:block"
-      style={{
-        transform:
-          scrollDirection === "down" ? "translateY(0)" : "translateY(64px)",
-      }}
-    >
+    <Card className="hidden lg:block">
       <CardHeader>
         <ProductChips variant="desktop" />
       </CardHeader>
