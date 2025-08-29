259176ea985aa89ae60f43ea25780f8c6d813d8f
nomaan-07 <nomaan07.dev@gmail.com>
2025-08-29 05:26:26 +0330

    feat: Add static cart popover UI

--- app/_components/Header/components/CartPanel/components/CartPopover.tsx
+++ app/_components/Header/components/CartPanel/components/CartPopover.tsx
@@ -1,7 +1,27 @@
+import FinalPrice from "@/app/_components/Common/FinalPrice";
+import { Button } from "@heroui/button";
+import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
+import Link from "next/link";
+
 function CartPopover() {
   return (
     <div className="invisible absolute top-9.5 left-0 hidden pt-2 opacity-0 transition-all group-hover:visible group-hover:opacity-100 lg:block">
-      <div className="border-small rounded-small border-default-300 bg-background w-72 px-1 py-2"></div>
+      <Card className="border-small rounded-small border-default-300 divide-default-200 w-96 divide-y px-1 py-2">
+        <CardHeader>
+          <span className="text-default-500 text-sm">۴ کالا</span>
+        </CardHeader>
+        <CardBody className="text-right">محصول</CardBody>
+        <CardFooter className="justify-between">
+          <div>
+            <span className="text-default-500 text-sm">مبلغ قابل پرداخت</span>
+            <FinalPrice price={4535000} variant="panel" />
+          </div>
+
+          <Button as={Link} href="/cart" color="primary" size="lg">
+            ثبت سفارش
+          </Button>
+        </CardFooter>
+      </Card>
     </div>
   );
 }
