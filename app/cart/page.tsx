23074205d7a408e4295c0257fec88d0143373ddf
nomaan-07 <nomaan07.dev@gmail.com>
2025-08-31 23:22:49 +0330

    feat: cart page title

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -2,7 +2,10 @@ import Cart from "../_components/Cart";
 
 function CartPage() {
   return (
-    <div className="mx-auto mt-4 max-w-7xl space-y-8 px-6">
+    <div className="mx-auto mt-4 max-w-7xl space-y-2 px-6 sm:space-y-6">
+      <div className="after:bg-default relative w-fit after:absolute after:top-7 after:h-0.5 after:w-full after:ring-0 after:content-[''] sm:after:top-8 md:after:top-9">
+        <h1 className="text-lg font-black sm:text-xl md:text-2xl">سبد خرید</h1>
+      </div>
       <Cart variant="page" />
     </div>
   );
