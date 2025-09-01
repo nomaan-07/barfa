d84defe5dde37b1ca49b6194cc2a7e03d67275dc
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-01 10:37:15 +0330

    feat: Cart layout

---
+++ app/cart/layout.tsx
@@ -0,0 +1,17 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "سبد خرید",
+  description:
+    "بررسی و مدیریت محصولات انتخاب‌شده در فروشگاه اینترنتی برفا. قبل از ثبت سفارش، قیمت نهایی و جزئیات محصولات خود را مشاهده کنید.",
+};
+
+function CartLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <>{children}</>;
+}
+
+export default CartLayout;
--- app/search/layout.tsx
+++ app/search/layout.tsx
@@ -1,9 +1,9 @@
-export function generateMetadata() {
-  return {
-    title: "جستجوی محصولات",
-    description: "جستجو در فروشگاه اینترنتی برفا برای محصولات دیجیتال",
-  };
-}
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "جستجوی محصولات",
+  description: "جستجو در فروشگاه اینترنتی برفا برای محصولات دیجیتال",
+};
 
 function SearchLayout({
   children,
