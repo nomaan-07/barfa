fa6697599ed27c2e8e073c7406dac301044fa18b
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-04 08:56:14 +0330

    refactor: middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,36 @@
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+import { supabase } from "./app/_lib/supabase";
+import { TABLES } from "./app/_utils/constants";
 
 const protectRoutes = ["/account", "/checkout"];
 const authRoutes = ["/login", "/signup"];
 
 export default async function middleware(req: NextRequest) {
+  const sessionId = (await cookies()).get("session")?.value;
   const path = req.nextUrl.pathname;
+  const isAuthRoute = authRoutes.includes(path);
   const isProtectedRoute = protectRoutes.some((route) =>
     path.startsWith(route),
   );
-  const isAuthRoute = authRoutes.includes(path);
 
-  const sessionId = (await cookies()).get("session")?.value;
+  let isAuthenticated = false;
+  if (sessionId) {
+    const { data: session } = await supabase
+      .from(TABLES.SESSIONS)
+      .select("expires_at")
+      .eq("id", sessionId)
+      .single();
 
-  if (isProtectedRoute && !sessionId) {
+    if (session && new Date(session.expires_at) > new Date()) {
+      isAuthenticated = true;
+    }
+  }
+  if (isProtectedRoute && !isAuthenticated) {
     return NextResponse.redirect(new URL("/login", req.nextUrl));
   }
 
-  if (isAuthRoute && sessionId) {
+  if (isAuthRoute && isAuthenticated) {
     return NextResponse.redirect(new URL("/", req.nextUrl));
   }
 
