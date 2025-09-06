840925f6d7e40bff9e327f23d634b4930be028bd
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-06 06:49:54 +0330

    refactor(cart): simplify Cart rendering

--- app/_components/Cart/index.tsx
+++ app/_components/Cart/index.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { selectorCartCount, useCartStore } from "@/app/_store/cartStore";
+import { useEffect, useState } from "react";
 import NoProductsFound from "../Common/ProductsList/components/NoProductsFound";
 import { variantClasses } from "./cartVariants";
 import CartContent from "./components/CartContent";
-import { CartBaseProps } from "./types";
-import { useEffect, useState } from "react";
 import CartSkeleton from "./components/CartSkeleton";
+import { CartBaseProps } from "./types";
 
 function Cart({ variant }: CartBaseProps) {
   const productsCount = useCartStore(selectorCartCount);
@@ -17,17 +17,15 @@ function Cart({ variant }: CartBaseProps) {
   }, []);
 
   if (!hydrated) return <CartSkeleton variant={variant} />;
-  if (productsCount === 0 && variant === "panel") return null;
 
   return (
     <div className={variantClasses.cartWrapper[variant]}>
       {productsCount > 0 ? (
         <CartContent variant={variant} />
       ) : (
-        <NoProductsFound
-          size={variant === "page" ? "lg" : "sm"}
-          title="سبد خرید شما خالی است."
-        />
+        variant === "page" && (
+          <NoProductsFound size="lg" title="سبد خرید شما خالی است." />
+        )
       )}
     </div>
   );
