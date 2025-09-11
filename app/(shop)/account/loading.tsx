c3dc547c195a7e95c5a39b20b92d74cfd012f41e
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-11 08:15:15 +0330

    feat(account): mobile nav skeleton

--- app/(shop)/account/loading.tsx
+++ app/(shop)/account/loading.tsx
@@ -1,11 +1,20 @@
 import AccountFormSkeleton from "@/app/_components/Account/components/skeleton/AccountFormSkeleton";
-import AccountWrapperSkeleton from "@/app/_components/Account/components/skeleton/AccountWrapperSkeleton";
+import AccountHeaderSkeleton from "@/app/_components/Account/components/skeleton/AccountHeaderSkeleton";
+import AccountMobileNavSkeleton from "@/app/_components/Account/components/skeleton/AccountMobileNavSkeleton";
+import AccountSidebarSkeleton from "@/app/_components/Account/components/skeleton/AccountSidebarSkeleton";
 
 function AccountLoading() {
   return (
-    <AccountWrapperSkeleton>
-      <AccountFormSkeleton />
-    </AccountWrapperSkeleton>
+    <div className="mx-auto mt-4 max-w-7xl space-y-8 px-6">
+      <div className="flex flex-col gap-4 md:flex-row-reverse">
+        <div className="w-full space-y-4">
+          <AccountHeaderSkeleton />
+          <AccountFormSkeleton />
+        </div>
+        <AccountSidebarSkeleton />
+      </div>
+      <AccountMobileNavSkeleton />
+    </div>
   );
 }
 
