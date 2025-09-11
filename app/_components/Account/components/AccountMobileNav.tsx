9b2bceaf083e5d1cb8f47a9a5f617a689e87f522
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-11 09:09:43 +0330

    refactor(account): remove skeletons for sidebar and mobile nav

--- app/_components/Account/components/AccountMobileNav.tsx
+++ app/_components/Account/components/AccountMobileNav.tsx
@@ -4,12 +4,9 @@ import { ACCOUNT_ITEMS } from "@/app/_data/accountItems";
 import { usePathname } from "next/navigation";
 import AccountMobileNavItem from "./AccountMobileNavItem";
 import MobileLogoutButton from "./MobileLogoutButton";
-import AccountMobileNavSkeleton from "./skeleton/AccountMobileNavSkeleton";
 
 function AccountMobileNav() {
   const pathname = usePathname();
-
-  if (!pathname) return <AccountMobileNavSkeleton />;
 
   return (
     <div className="border-t-default-200 fixed right-0 bottom-0 left-0 z-50 flex justify-between border-t bg-white p-2 lg:hidden">
--- app/_components/Account/components/AccountSidebar.tsx
+++ app/_components/Account/components/AccountSidebar.tsx
@@ -3,17 +3,14 @@
 import { Card, CardBody } from "@heroui/card";
 import { usePathname } from "next/navigation";
 import AccountMenu from "./AccountMenu";
-import AccountSidebarSkeleton from "./skeleton/AccountSidebarSkeleton";
 
 function AccountSidebar() {
   const pathname = usePathname();
 
-  if (!pathname) return <AccountSidebarSkeleton />;
-
   return (
     <Card className="hidden w-64 shrink-0 lg:block">
       <CardBody>
-        <AccountMenu pathname={pathname} size="lg" />
+        <AccountMenu pathname={pathname || "/"} size="lg" />
       </CardBody>
     </Card>
   );
