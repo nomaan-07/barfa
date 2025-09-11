000940e22ac7ddbb58303d94f0d3848f02d7d0a9
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-11 08:53:36 +0330

    fix(account): remove root loading.tsx and add skeletons for sidebar/mobile nav hydration

--- app/_components/Account/components/AccountSidebar.tsx
+++ app/_components/Account/components/AccountSidebar.tsx
@@ -3,9 +3,12 @@
 import { Card, CardBody } from "@heroui/card";
 import { usePathname } from "next/navigation";
 import AccountMenu from "./AccountMenu";
+import AccountSidebarSkeleton from "./skeleton/AccountSidebarSkeleton";
 
 function AccountSidebar() {
   const pathname = usePathname();
+
+  if (!pathname) return <AccountSidebarSkeleton />;
 
   return (
     <Card className="hidden w-64 shrink-0 lg:block">
