a1d56d6ce37ada1b792c968d7b67d63f2ffdc6cb
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-13 18:44:49 +0330

    feat(category): enable smooth scroll to top on page load

--- app/_components/Common/ScrollToTop.tsx
+++ app/_components/Common/ScrollToTop.tsx
@@ -4,7 +4,10 @@ import { useEffect } from "react";
 
 function ScrollToTop() {
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }, []);
   return null;
 }
