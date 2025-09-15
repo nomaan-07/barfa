06ddabfb7cb6797eb5d222f1ec30d90b9dccf34e
nomaan-07 <nomaan07.dev@gmail.com>
2025-09-15 23:38:56 +0330

    fix: terms link

--- app/_components/Auth/components/AuthTerms.tsx
+++ app/_components/Auth/components/AuthTerms.tsx
@@ -4,7 +4,7 @@ function AuthTerms() {
   return (
     <div className="text-center text-sm">
       با عضویت در سایت، همه‌ی{" "}
-      <Link href="/terms" className="text-primary">
+      <Link href="/" className="text-primary">
         قوانین و شرایط
       </Link>{" "}
       برفا را پذیرفته‌اید.
